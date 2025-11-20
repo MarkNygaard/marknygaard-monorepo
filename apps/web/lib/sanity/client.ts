7bf32e9ba8d4b3e4c946b0c716e37c0a30a24279
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-11-20 19:40:56 +0100

    add read token back to client

--- apps/web/lib/sanity/client.ts
+++ apps/web/lib/sanity/client.ts
@@ -7,6 +7,7 @@ export const client = createClient({
   projectId: sanity.projectId,
   dataset: sanity.dataset,
   apiVersion: sanity.apiVersion,
+  token: process.env.SANITY_API_READ_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   stega: {
     studioUrl: sanity.studioUrl,
